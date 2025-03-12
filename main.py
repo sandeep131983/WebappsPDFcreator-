d9ca9d04362ae2f763f9039e948e48a8533534ca
sandy131983 <sandeep.yerramareddigari@gmail.com>
2025-03-12 15:29:24 -0400

    Basic Pdf

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from fpdf import FPDF
 import pandas as pd
 
 df=pd.read_csv("topics.csv")
-
 pdf=FPDF(orientation="P",format="A4",unit="mm")
-pdf.add_page()
-pdf.set_font(family="Times",style="B",size=10)
-
-pdf.cell(w=0,h=10,txt="This is pdf",align="L",border=1,ln=1)
-pdf.set_font(family="Times",style="",size=10)
-pdf.cell(w=0,h=10,txt="This is second line in pdf",align="L",border=1,ln=1)
-
+for index,page in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times",style="B",size=10)
+    pdf.set_text_color(254, 110, 120)
+    pdf.cell(w=0,h=10,txt=page["Topic"],align="L",ln=1)
+    pdf.line(10,17,200,17)
 pdf.output("output.pdf")
+
+
 
